c36471aa5ed80cb92012810617c1ff10ba50e77d
leowipin <leonardopincayjaramillo@gmail.com>
2025-01-14 11:43:19 -0500

    new program

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,16 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication1;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
-//db config
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -19,7 +19,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 //pipeline
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
@@ -29,111 +31,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//using Microsoft.EntityFrameworkCore;
-//using WebApplication1;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//builder.Services.AddControllers();
-
-////swagger config
-//builder.Services.AddEndpointsApiExplorer();
-
-//builder.Services.AddSwaggerGen();
-
-//builder.Services.AddDbContext<ApplicationDbContext>(options => 
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-//var app = builder.Build();
-
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthentication();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-////using Microsoft.EntityFrameworkCore;
-////using WebApplication1;
-
-////var builder = WebApplication.CreateBuilder(args);
-
-////// Add services to the container.
-
-////builder.Services.AddControllers();
-////// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-////builder.Services.AddEndpointsApiExplorer();
-////builder.Services.AddSwaggerGen();
-
-////// database config
-////builder.Services.AddDbContext<ApplicationDbContext>(options => 
-////    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-////var app = builder.Build();
-
-////// Configure the HTTP request pipeline.
-////if (app.Environment.IsDevelopment())
-////{
-////    app.UseSwagger();
-////    app.UseSwaggerUI();
-////}
-
-////app.UseHttpsRedirection();
-
-////app.UseAuthorization();
-
-////app.MapControllers();
-
-////app.Run();
